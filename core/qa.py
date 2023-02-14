41aa0c07de5e080c8713cd36ff99f2cc48b2941f
misa <misa@rd-vdi-ub-017.rd.local>
2023-02-14 17:16:03 +0700

    25

--- core/qa.py
+++ core/qa.py
@@ -19,6 +19,21 @@ def param_input():
         'top_p', min_value = 0.0, max_value = 1.0, value = COMPLETIONS_API_PARAMS['top_p'], step = 0.01
     )
 
+def parse_response(response):
+    response = response.replace(r'\n', '\n\n')
+    links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
+    if len(links) > 0:
+        for link in links:
+            link = link.replace('[', '').replace(']', '')
+            if link[-4:] in ['.jpg', '.png', 'jpeg', '.gif']:
+                response = response.replace('[' + link + ']', f'![image]({link})')
+            if link.startswith('https://vimeo.com'):
+                id = link[link.rindex('/') + 1:]
+                response = response.replace('[' + link + ']', f'<iframe src="https://player.vimeo.com/video/{id}?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>')
+            if link.startswith('https://www.youtube.com/embed/'):
+                response = response.replace('[' + link + ']', f'<iframe src="<iframe width="560" height="315" src="{link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+    return response
+
 def main():
     key = os.environ.get('OPENAI_KEY')
     if key is not None:
@@ -48,25 +63,21 @@ def main():
     prompt = construct_prompt(question)[0]
     context.markdown(prompt)
     if st.button('Lấy câu trả lời'):
-        tokens = count_tokens(prompt)
+        
+        if st.checkbox('Trả lời tiếp'):
+            prompt = construct_prompt(question)[0] + st.session_state.p
+            tokens = count_tokens(prompt)
+            response = st.session_state.p
+        else:
+            response = ''
+            tokens = count_tokens(prompt)
         REPLACE_API_PARAMS['max_tokens'] = 4096 - tokens
         with st.spinner('Đang sinh câu trả lời...'):
-            response = ''
             for resp in openai.Completion.create(prompt = prompt, **REPLACE_API_PARAMS):
                 tokens += 1
                 response += resp.choices[0].text
-                response = response.replace(r'\n', '\n\n')
-                links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
-                if len(links) > 0:
-                    for link in links:
-                        link = link.replace('[', '').replace(']', '')
-                        if link[-4:] in ['.jpg', '.png', 'jpeg', '.gif']:
-                            response = response.replace('[' + link + ']', f'![image]({link})')
-                        if link.startswith('https://vimeo.com'):
-                            id = link[link.rindex('/') + 1:]
-                            response = response.replace('[' + link + ']', f'<iframe src="https://player.vimeo.com/video/{id}?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>')
-                        if link.startswith('https://www.youtube.com/embed/'):
-                            response = response.replace('[' + link + ']', f'<iframe src="<iframe width="560" height="315" src="{link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+                response = parse_response(response)
+                st.session_state.p = response
                 try:
                     answer.markdown(response, unsafe_allow_html = True)
                 except:
