8feef95e2bd15c230ffe2c91eb6f3d5bd5d7bd25
Vũ Mạnh Quang <quang.vu.1798@gmail.com>
2023-03-24 14:11:54 +0700

    Update process.py

--- core/process.py
+++ core/process.py
@@ -52,6 +52,7 @@ def order_document_sections_by_query_similarity(query: str, document_embeddings:
 
 
 def get_document(question: str, product = None):
+    url = 'https://test-aiservice.misa.com.vn/embedding-service/search/context'
     params = {
         "session_id": "",
         "product": "",
